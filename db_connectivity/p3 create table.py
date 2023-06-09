44af995f87208bab061fda2d1f338eff1a2483a7
Tejas adodariya <132728157+Tejas-adodariya@users.noreply.github.com>
2023-06-09 19:31:18 +0530

    Add files via upload

---
+++ db_connectivity/p3 create table.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "pythonmysql"
+
+)
+mycursor = mydb.cursor()
+mycursor.execute("create table tbl1(id int(11),nm varchar(20),city varchar(10))")
---
+++ db_connectivity/p4 insert data.py
@@ -0,0 +1,14 @@
+import mysql.connector
+try :
+    mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "pythonmysql"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("insert into tbl1 values(5,'python','hp')")
+    print("Record inserted successfully")
+except :
+    print("Error")
+mydb.commit()
